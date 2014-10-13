59146cc5d631e8404b36d7baeb728c87ae6bd9fb
Alyssa <alyssa@thepalesnail.com>
2014-10-13 11:28:01 -0400

    attempted changes?

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,11 @@ var API_URL = 'https://api.github.com/users/alyssaamrhein';
 // calling the url
 var withAjax = function () {
   $.ajax({url: API_URL}).done(function (data) {
-
+    // calls ajax and uses done method
     $('.info').text(JSON.stringify(data));
-
+    // turns whatever is returned into a string
     setProfileInfo(data);
+    $('#sidebar').html(_.template($('#sidebarTemplate').html())(data));
 
   }).fail(function (data) {
 
@@ -19,9 +20,32 @@ var withAjax = function () {
 };
 
 
+var API_URL2 = 'https://api.github.com/users/alyssaamrhein/repos';
+// calling the url
+var gettinRepo = function () {
+  $.ajax({url: API_URL2}).done(function (data) {
+
+    $('.info2').text(JSON.stringify(data));
+
+    setRepoInfo();
+
+
+  }).fail(function (data) {
+
+    $('.error').removeClass('fade').text(data.responseJSON.message);
+
+    setTimeout(function () {
+      $('.error').addClass('fade');
+    }, 1000);
+
+  });
+};
+
+
+
 var setProfileInfo = function (profileData) {
-  $('.name').text(profileData.name);
-  $('.login').text(profileData.login);
+  $('#name').text(profileData.name);
+  $('#login').text(profileData.login);
   $('#created_at').text(profileData.created_at);
   $('#avatar').html("<img src='" + profileData.avatar_url + "'/>");
   $('#location').text(profileData.location);
@@ -32,55 +56,20 @@ var setProfileInfo = function (profileData) {
   $('#email').text(profileData.email);
 };
 
-withAjax();
-
-/*
-var results = {
-  "login": "alyssaamrhein",
-  "id": 5555065,
-  "avatar_url": "https://avatars.githubusercontent.com/u/5555065?v=2",
-  "gravatar_id": "",
-  "url": "https://api.github.com/users/alyssaamrhein",
-  "html_url": "https://github.com/alyssaamrhein",
-  "followers_url": "https://api.github.com/users/alyssaamrhein/followers",
-  "following_url": "https://api.github.com/users/alyssaamrhein/following{/other_user}",
-  "gists_url": "https://api.github.com/users/alyssaamrhein/gists{/gist_id}",
-  "starred_url": "https://api.github.com/users/alyssaamrhein/starred{/owner}{/repo}",
-  "subscriptions_url": "https://api.github.com/users/alyssaamrhein/subscriptions",
-  "organizations_url": "https://api.github.com/users/alyssaamrhein/orgs",
-  "repos_url": "https://api.github.com/users/alyssaamrhein/repos",
-  "events_url": "https://api.github.com/users/alyssaamrhein/events{/privacy}",
-  "received_events_url": "https://api.github.com/users/alyssaamrhein/received_events",
-  "type": "User",
-  "site_admin": false,
-  "name": "alyssa",
-  "company": "",
-  "blog": "",
-  "location": "",
-  "email": "",
-  "hireable": false,
-  "bio": null,
-  "public_repos": 10,
-  "public_gists": 0,
-  "followers": 0,
-  "following": 0,
-  "created_at": "2013-09-27T04:49:55Z",
-  "updated_at": "2014-10-10T22:06:08Z"
+var setRepoInfo = function (data) {
+  _.each(data, function (repo) {
+    var repoTemplateString = $('#tabTemplate').html();
+    var repoTemplate = _.template(repoTemplateString);
+    var finishedRepoTemplate = repoTemplate(repo);
+    $('#popRop').append(finishedRepoTemplate);
+  });
 };
-*/
 
 
-$('#nameContainer').html(
-  _.template($('#nameTemplate').html())(results)
-);
 
-$('#loginContainer').html(
-  _.template($('#loginTemplate').html())(results)
-);
+withAjax();
+gettinRepo();
 
-$('#createdContainer').html(
-  _.template($('#createdTemplate').html())(results)
-);
 
 /* Tab JS */
 
